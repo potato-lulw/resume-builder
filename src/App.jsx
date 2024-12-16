87bfafd3408faced815ac7cc718cbdd205fa9787
potato-lulw <om.patil17072@gmail.com>
2024-12-17 02:36:18 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Button } from "./components/ui/button"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+
+        </Routes>
+      </Router>
+
+    </>
+  )
+}
+
+export default App
